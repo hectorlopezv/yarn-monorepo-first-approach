dd807c7e2c51cd303ed8fec17b0c93b8815ee980
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2021-07-29 23:10:06 -0500

    how to import files, ... import .tsx/ts file directly from source folder(lib)

--- __tests__/prueba.test.js
+++ __tests__/prueba.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {render, screen} from '@testing-library/react'
 import user from '@testing-library/user-event'
 import {andres} from '@libprov/test'
-import { Smilef } from '@libProv/smilef'
+import { Smilef } from '@libProv/smilef/lib/smilef'
 describe('testing suite', () => {
   it('testing unit test1', () => {
    const {debug} =  render(<Smilef sad title="hector"/>)
