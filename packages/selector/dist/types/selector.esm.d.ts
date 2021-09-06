c5b44961daa371cdd3a3fb90ae849ba01ed87f60
hvmlopez <hectorvmlopez@gmail..com>
2021-09-06 15:12:34 -0500

    Publish
     - @libprov/input@1.0.4
     - @libprov/proveedores@2.0.7
     - @libprov/selector@2.0.7

--- packages/selector/dist/types/selector.esm.d.ts
+++ packages/selector/dist/types/selector.esm.d.ts
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 interface optionInterface {
-  value: string
-  label: string
+    value: string;
+    label: string;
 }
 interface SelectProps {
-  styles?: any
-  label: string
-  disabled: boolean
-  placeholder: string
-  Options?: optionInterface[]
-  value?: string
-  type?: string
-  onClick?: (args: any, type: string) => void
-  className?: string
-  inputId?: string
+    styles?: any;
+    label: string;
+    disabled: boolean;
+    placeholder: string;
+    Options?: optionInterface[];
+    value?: string;
+    type?: string;
+    onClick?: (args: any, type: string) => void;
+    className?: string;
+    inputId?: string;
 }
-declare const Selector: React.FC<SelectProps>
-export {optionInterface, SelectProps, Selector}
+declare const Selector: React.FC<SelectProps>;
+export { optionInterface, SelectProps, Selector };
 //# sourceMappingURL=selector.esm.d.ts.map
