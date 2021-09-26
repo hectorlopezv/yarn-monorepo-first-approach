28cbff66f53902e19b5824bbf9ed54b7eb2ba0bd
hvmlopez <hectorvmlopez@gmail..com>
2021-09-26 13:55:41 -0500

    Publish
     - @libprov/dragAndDropFile@1.0.1
     - @libprov/fileList@1.0.1
     - @libprov/input@1.0.5
     - @libprov/pdf@2.0.5
     - @libprov/proveedores@2.0.10

--- packages/fileList/dist/types/fileList.cjs.d.ts
+++ packages/fileList/dist/types/fileList.cjs.d.ts
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 interface pdfItem {
-  name: string
-  pdf: string
-  icons: React.ReactNode
+    name: string;
+    pdf: string;
+    icons: React.ReactNode;
 }
 interface fileListProps {
-  pdfs: pdfItem[]
-  title: string
+    pdfs: pdfItem[];
+    title: string;
 }
-declare const FileList: React.FC<fileListProps>
-export {FileList}
+declare const FileList: React.FC<fileListProps>;
+export { FileList };
 //# sourceMappingURL=fileList.cjs.d.ts.map
--- packages/input/dist/types/input.cjs.d.ts
+++ packages/input/dist/types/input.cjs.d.ts
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 interface InputSimpleProps {
-  labelInput: string
-  maxLength: number
-  value: string
-  disabled: boolean
-  placeholder: string
-  type?: string
-  onChange: (...args: unknown[]) => void
-  onKeyDown: (...args: unknown[]) => void
+    labelInput: string;
+    maxLength: number;
+    value: string;
+    disabled: boolean;
+    placeholder: string;
+    type?: string;
+    onChange: (...args: unknown[]) => void;
+    onKeyDown: (...args: unknown[]) => void;
 }
-declare const InputSimple: React.FC<InputSimpleProps>
-export {InputSimple as default, InputSimple, InputSimpleProps}
+declare const InputSimple: React.FC<InputSimpleProps>;
+export { InputSimple as default, InputSimple, InputSimpleProps };
 //# sourceMappingURL=input.cjs.d.ts.map
